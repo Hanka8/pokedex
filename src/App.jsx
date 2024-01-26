65dcf43a2df5f2faaa073144f06ab4bd7f5448c5
Hanka8 <hanka.maruskevicova@gmail.com>
2024-01-26 17:10:35 +0100

    results per page select added

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,26 @@ import React, { useState, useEffect } from 'react';
 import PokemonList from './PokemonList';
 
 import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import NativeSelect from '@mui/material/NativeSelect';
 
 function App() { 
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(true);
-
   const [pokemonData, setPokemonData] = useState([]);
+  const [resultsPerPage, setResultsPerPage] = useState(20);
 
   useEffect(() => {
   setLoading(true);
 
-  axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${(currentPage - 1) * 20}&limit=20`)
+  axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${(currentPage - 1) * resultsPerPage}&limit=${resultsPerPage}`)
     .then(res => {
       setLoading(false);
-      setTotalPages(Math.ceil(res.data.count / 20));
+      setTotalPages(Math.ceil(res.data.count / resultsPerPage));
 
       // Use Promise.all to wait for all Pokemon details requests
       const pokemonDetailsPromises = res.data.results.map(p =>
@@ -47,29 +51,49 @@ function App() {
     .catch(error => {
       console.error('Error:', error.message);
     });
-}, [currentPage]);
+}, [currentPage, resultsPerPage]);
 
 
   function handlePageChange(event, newPage) {
     setCurrentPage(newPage);
   }
 
-  if (loading) return "Loading...";
+  function handleResultsPerPageChange(event) {
+    setResultsPerPage(event.target.value);
+  }
 
   return (
     <>
+    <FormControl sx={{ m: 1, minWidth: 220 }} size="small">
+      <InputLabel variant="standard" htmlFor="uncontrolled-native">
+        Results per page
+      </InputLabel>
+      <NativeSelect
+        defaultValue={20}
+        inputProps={{
+          name: 'age',
+          id: 'uncontrolled-native',
+        }}
+        onChange={handleResultsPerPageChange}
+      >
+        <option value={10}>10</option>
+        <option value={20}>20</option>
+        <option value={30}>30</option>
+      </NativeSelect>
+    </FormControl>
+
       <PokemonList 
         pokemonData={pokemonData} 
       />
 
-      <Stack spacing={2} mt={2}>
-        <Pagination
-          count={totalPages}
-          page={currentPage}
-          onChange={handlePageChange}
-          color="primary"
-        />
-      </Stack>
+      <Pagination
+        className='pagination'
+        count={totalPages}
+        page={currentPage}
+        onChange={handlePageChange}
+        color="primary"
+      />
+
     </>
   );
 }
